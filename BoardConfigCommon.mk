c263cee61dd0a6fe2504b23618938633dfc630ba
spkal01 <kalligeross@gmail.com>
2023-05-22 10:24:21 +0300

    Sm6150: Switch to jemalloc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,6 +74,10 @@ BOARD_HAVE_QCOM_FM := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
 
+MALLOC_SVELTE := true
+
+MALLOC_SVELTE_FOR_LIBC32 := true
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
